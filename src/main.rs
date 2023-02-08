1003a189c09a1336d6677155ff19b053f21b6314
Kostiantyn Shyrolapov <kostya.shirolapov@gmail.com>
2023-02-08 01:09:32 -0500

    add random map generation, redraw on keypress

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use cursive::views::{
     TextView, ViewRef,
 };
 use cursive::{align, event::Key, Cursive, CursiveExt};
+use rand::Rng;
+
 fn main() {
     let mut c = cursive::default();
 
     let mut runner = c.runner();
+
     runner.load_toml(include_str!("./theme.toml")).unwrap();
 
     runner.add_global_callback('q', |s| s.quit());
@@ -16,7 +19,9 @@ fn main() {
     // Start menu
     runner.add_layer(cursive::views::Dialog::around(
         cursive::views::LinearLayout::vertical()
-            .child(cursive::views::Button::new("New game", |s| {}))
+            .child(cursive::views::Button::new("New game", move |s| {
+                new_game(s);
+            }))
             .child(cursive::views::Button::new("Settings", |s| {}))
             .child(cursive::views::Button::new("Quit", |s| s.quit())),
     ));
@@ -57,6 +62,7 @@ fn main() {
         );
 
     runner.add_global_callback(Key::Esc, |s| s.select_menubar());
+
     while runner.is_running() {
         runner.refresh();
 
@@ -67,4 +73,53 @@ fn main() {
 fn showpopup(s: &mut Cursive, name: &str) {
     s.pop_layer();
     s.add_layer(Dialog::info(format!("Hello {}!", name)));
+}
+
+fn new_game(s: &mut Cursive) {
+    s.pop_layer();
+
+    s.add_layer(
+        Dialog::around(
+            ListView::new()
+                .child(
+                    "Heigth: ",
+                    EditView::new().with_name("set height").fixed_width(3),
+                )
+                .child(
+                    "Width",
+                    EditView::new().with_name("set width").fixed_width(3),
+                ),
+        )
+        .button("Start", |s| {
+            let height = s
+                .call_on_name("set height", |view: &mut EditView| view.get_content())
+                .unwrap();
+            let width = s
+                .call_on_name("set width", |view: &mut EditView| view.get_content())
+                .unwrap();
+            draw_map(s, height.parse().unwrap(), width.parse().unwrap());
+        }),
+    );
+}
+
+fn draw_map(s: &mut Cursive, height: i32, width: i32) {
+    s.pop_layer();
+    s.add_layer(Dialog::info(construct_map(height, width)));
+    s.add_global_callback('f', move |s| {
+        s.pop_layer();
+        s.add_layer(Dialog::info(construct_map(height, width)));
+        s.cb_sink().send(Box::new(Cursive::noop)).unwrap();
+    })
+}
+
+fn construct_map(height: i32, width: i32) -> String {
+    let mut map = String::new();
+    for _ in 0..height {
+        for _ in 0..width {
+            let x: &str = if rand::random() { "*" } else { "_" };
+            map.push_str(x);
+        }
+        map.push_str("\n");
+    }
+    map
 }
