8e46d9252a99a40426c8eff1f3183289e47a0ca6
Kostiantyn Shyrolapov <kostya.shirolapov@gmail.com>
2023-02-08 21:34:37 -0500

    feat: collision detection, floors feature
    there's a lot to improve. getting used to Rust, code definitely wishes for modularity at this point

---
+++ README.md
@@ -0,0 +1,3 @@
+My first Rust project using Cursive TUI library.
+
+This is a simple game (aiming for it to be a roguelike RPG in the end) that I'm developing to learn Rust and game dev concepts.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use cursive::theme::Style;
+use cursive::view::Margins;
 use cursive::views::{
-    Button, Dialog, EditView, LinearLayout, ListView, MenuPopup, Menubar, Panel, SliderView,
-    TextView, ViewRef,
+    Button, Dialog, EditView, LinearLayout, ListView, MenuPopup, Menubar, Panel, SelectView,
+    SliderView, TextView, ViewRef,
 };
 use cursive::{align, event::Key, Cursive, CursiveExt};
 use cursive::{direction, traits::*};
@@ -10,10 +11,17 @@ use rand::Rng;
 struct PlayerLoc {
     x: i32,
     y: i32,
+    floor: i32,
+}
+
+#[derive(Clone)]
+struct GameMap {
+    tiles: [[char; 24 * 2 + 1]; 24],
 }
 
 struct UserData {
     player_loc: PlayerLoc,
+    map: GameMap,
     //    map: Vec<Vec<String>>,
 }
 
@@ -30,7 +38,7 @@ fn main() {
     runner.add_layer(cursive::views::Dialog::around(
         cursive::views::LinearLayout::vertical()
             .child(cursive::views::Button::new("New game", move |s| {
-                new_game(s);
+                draw_map(s);
             }))
             .child(cursive::views::Button::new("Settings", |s| {}))
             .child(cursive::views::Button::new("Quit", |s| s.quit())),
@@ -74,7 +82,14 @@ fn main() {
     runner.add_global_callback(Key::Esc, |s| s.select_menubar());
 
     runner.set_user_data(UserData {
-        player_loc: PlayerLoc { x: 0, y: 0 },
+        player_loc: PlayerLoc {
+            x: 1,
+            y: 1,
+            floor: 9,
+        },
+        map: GameMap {
+            tiles: [[' '; 24 * 2 + 1]; 24],
+        },
     });
 
     while runner.is_running() {
@@ -89,79 +104,120 @@ fn showpopup(s: &mut Cursive, name: &str) {
     s.add_layer(Dialog::info(format!("Hello {}!", name)));
 }
 
-fn new_game(s: &mut Cursive) {
-    s.pop_layer();
-
-    s.add_layer(
-        Dialog::around(
-            ListView::new()
-                .child(
-                    "Heigth: ",
-                    EditView::new().with_name("set height").fixed_width(3),
-                )
-                .child(
-                    "Width",
-                    EditView::new().with_name("set width").fixed_width(3),
-                ),
-        )
-        .button("Start", |s| {
-            let height = s
-                .call_on_name("set height", |view: &mut EditView| view.get_content())
-                .unwrap();
-            let width = s
-                .call_on_name("set width", |view: &mut EditView| view.get_content())
-                .unwrap();
-            draw_map(s, height.parse().unwrap(), width.parse().unwrap());
-        }),
-    );
-}
-
-fn draw_map(s: &mut Cursive, height: i32, width: i32) {
+fn draw_map(s: &mut Cursive) {
+    let height = 24;
+    let width = 24;
     let mut player_x = s.user_data::<UserData>().unwrap().player_loc.x;
     let mut player_y = s.user_data::<UserData>().unwrap().player_loc.y;
     s.pop_layer();
-    s.add_layer(TextView::new(construct_map(height, width, player_x, player_y)).with_name("map"));
+    s.add_layer(
+        LinearLayout::horizontal()
+            .child(
+                LinearLayout::vertical()
+                    .child(Dialog::around(TextView::new("Floor: 9").with_name("floor")))
+                    .child(Dialog::around(TextView::new("map").with_name("map"))),
+            )
+            .child(Dialog::around(
+                LinearLayout::vertical()
+                    .child(SelectView::new().item("Item 1", 1).item("Item 2", 2))
+                    .child(SelectView::new().item("Item 1", 1).item("Item 2", 2)),
+            )),
+    );
+
+    // s.find_name::<TextView>("map")
+    //     .unwrap()
+    //     .set_content(construct_map(s, height, width, player_x, player_y));
+
     s.add_global_callback('w', move |s| move_player(s, height, width, "north"));
     s.add_global_callback('a', move |s| move_player(s, height, width, "west"));
     s.add_global_callback('s', move |s| move_player(s, height, width, "south"));
     s.add_global_callback('d', move |s| move_player(s, height, width, "east"));
 }
 
-fn construct_map(height: i32, width: i32, player_x: i32, player_y: i32) -> String {
-    let mut map = String::new();
+fn construct_map(s: &mut Cursive, height: i32, width: i32, player_x: i32, player_y: i32) -> String {
     for i in 0..height {
-        for j in 0..width * 2 {
-            let x: &str = if i == player_y && j == player_x * 2 {
-                "*"
+        for j in 0..width * 2 + 1 {
+            let x: char;
+            let current_tile = s.user_data::<UserData>().unwrap().map.tiles[i as usize][j as usize];
+            let y_axis_stairs_up_placement = 11..13;
+            let y_axis_stairs_down_placement = 14..16;
+            let x_axis_stairs_placement = 8..10;
+
+            if i == player_y && j == player_x * 2 {
+                x = '*'; // player
+            } else if y_axis_stairs_down_placement.contains(&i)
+                && (x_axis_stairs_placement.contains(&j))
+            {
+                x = '>';
+            } else if y_axis_stairs_down_placement.contains(&i)
+                && (x_axis_stairs_placement.contains(&(j - 1)))
+            {
+                x = 'v';
+            } else if y_axis_stairs_up_placement.contains(&i)
+                && (x_axis_stairs_placement.contains(&j))
+            {
+                x = '>';
+            } else if y_axis_stairs_up_placement.contains(&i)
+                && (x_axis_stairs_placement.contains(&(j - 1)))
+            {
+                x = '^';
+            } else if i == 0
+                || (i == 4 && (j >= 8 && j <= width * 2 - 8))
+                || (i == height - 5 && (j >= 8 && j <= width * 2 - 8))
+                || i == height - 1
+                || j == 0
+                || (j == 8 && (i >= 4 && i <= height - 5))
+                || (j == width * 2 - 8 && (i >= 4 && i <= height - 5))
+                || j == width * 2
+                || (x_axis_stairs_placement.contains(&(j - 1))
+                    && (y_axis_stairs_down_placement.contains(&(i - 1))
+                        || y_axis_stairs_down_placement.contains(&(i + 1))
+                        || y_axis_stairs_up_placement.contains(&(i + 1))))
+            {
+                x = '#'; // walls
+            } else if current_tile == ' ' && current_tile == '*' {
+                x = current_tile;
             } else {
-                " "
-            };
-            map.push_str(x);
+                x = ' '; // empty space
+            }
+
+            s.user_data::<UserData>().unwrap().map.tiles[i as usize][j as usize] = x;
         }
-        map.push_str("\n");
     }
-    map
+    let map = s.user_data::<UserData>().unwrap().map.tiles;
+    let mut map_string = String::new();
+    for rows in map.iter() {
+        for cols in rows.iter() {
+            map_string.push_str(cols.to_string().as_str());
+        }
+        map_string.push_str("\n");
+    }
+
+    map_string
 }
 
 fn move_player(s: &mut Cursive, height: i32, width: i32, direction: &str) {
+    let player_x = s.user_data::<UserData>().unwrap().player_loc.x;
+    let player_y = s.user_data::<UserData>().unwrap().player_loc.y;
+    let tiles = s.user_data::<UserData>().unwrap().map.tiles;
     match direction {
         "north" => {
-            if s.user_data::<UserData>().unwrap().player_loc.y > 0 {
+            if player_y > 0 && tiles[player_y as usize - 1][player_x as usize * 2] != '#' {
                 s.user_data::<UserData>().unwrap().player_loc.y -= 1
             };
         }
         "south" => {
-            if s.user_data::<UserData>().unwrap().player_loc.y < height - 1 {
+            if player_y < height - 1 && tiles[player_y as usize + 1][player_x as usize * 2] != '#' {
                 s.user_data::<UserData>().unwrap().player_loc.y += 1
             };
         }
         "east" => {
-            if s.user_data::<UserData>().unwrap().player_loc.x < width - 1 {
+            if player_x < width - 1 && tiles[player_y as usize][player_x as usize * 2 + 2] != '#' {
                 s.user_data::<UserData>().unwrap().player_loc.x += 1
             };
         }
         "west" => {
-            if s.user_data::<UserData>().unwrap().player_loc.x > 0 {
+            if player_x > 0 && tiles[player_y as usize][player_x as usize * 2 - 2] != '#' {
                 s.user_data::<UserData>().unwrap().player_loc.x -= 1
             };
         }
@@ -169,7 +225,57 @@ fn move_player(s: &mut Cursive, height: i32, width: i32, direction: &str) {
     }
     let player_x = s.user_data::<UserData>().unwrap().player_loc.x;
     let player_y = s.user_data::<UserData>().unwrap().player_loc.y;
+
+    let y_axis_stairs_up_placement = 11..13;
+    let y_axis_stairs_down_placement = 14..16;
+    let x_axis_stairs_placement = 8..10;
+
+    if player_x == 5 && y_axis_stairs_up_placement.contains(&player_y) {
+        s.user_data::<UserData>().unwrap().player_loc.x = 3;
+        s.user_data::<UserData>().unwrap().player_loc.y = 14;
+        s.user_data::<UserData>().unwrap().player_loc.floor += 1;
+        floor_gen(s);
+    } else if player_x == 5 && y_axis_stairs_down_placement.contains(&player_y) {
+        s.user_data::<UserData>().unwrap().player_loc.x = 3;
+        s.user_data::<UserData>().unwrap().player_loc.y = 11;
+        s.user_data::<UserData>().unwrap().player_loc.floor -= 1;
+        floor_gen(s);
+    } else if x_axis_stairs_placement.contains(&player_x) && player_y == 11 {
+        s.user_data::<UserData>().unwrap().player_loc.x = 3;
+        s.user_data::<UserData>().unwrap().player_loc.y = 14;
+        s.user_data::<UserData>().unwrap().player_loc.floor += 1;
+        floor_gen(s);
+    } else if x_axis_stairs_placement.contains(&player_x) && player_y == 14 {
+        s.user_data::<UserData>().unwrap().player_loc.x = 3;
+        s.user_data::<UserData>().unwrap().player_loc.y = 11;
+        s.user_data::<UserData>().unwrap().player_loc.floor -= 1;
+        floor_gen(s);
+    }
+
+    let player_x = s.user_data::<UserData>().unwrap().player_loc.x;
+    let player_y = s.user_data::<UserData>().unwrap().player_loc.y;
+
     s.find_name::<TextView>("map")
         .unwrap()
-        .set_content(construct_map(height, width, player_x, player_y));
+        .set_content(construct_map(s, height, width, player_x, player_y));
+    s.find_name::<TextView>("floor")
+        .unwrap()
+        .set_content(format!(
+            "Floor: {}",
+            s.user_data::<UserData>().unwrap().player_loc.floor
+        ));
+}
+
+fn floor_gen(s: &mut Cursive) {
+    let rng = rand::thread_rng().gen_range(0..100);
+    let tiles = s.user_data::<UserData>().unwrap().map.tiles;
+
+    if rng < 50 {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(0..tiles.len());
+        let y = rng.gen_range(0..tiles[x].len());
+        if tiles[x][y] == ' ' {
+            s.user_data::<UserData>().unwrap().map.tiles[x][y] = 'M';
+        }
+    }
 }
